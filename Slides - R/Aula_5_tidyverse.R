633af83a7c14c25f0e374aac48dd41fc7f6d3a1e
Leonardo F. Nascimento <leofn3@gmail.com>
2021-10-06 18:02:28 -0300

    aula5

---
+++ Slides - R/Aula_5_tidyverse.R
@@ -0,0 +1,26 @@
+#remotes::install_github("cienciadedatos/dados")
+library(dados)
+library(tidyverse)
+
+df <- dados_starwars # como tibble
+df <- as.data.frame(dados_starwars) # como data.frame
+
+
+# Vamos ver a nossa base de uma forma mais legal?
+
+glimpse(df)
+
+
+# 1. filter
+
+df 
+
+
+# Qual a média de altura dos personagens Masculinos?
+
+df %>% 
+  filter(genero == "Masculino") %>% 
+  #drop_na() %>% 
+  group_by(especie) %>% 
+  summarise(mean(altura))
+ 
