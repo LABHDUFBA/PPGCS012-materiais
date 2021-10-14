84610963412c3b81ff2acced244e7a2fddee6ac8
ericbrasiln <profericbrasil@unilab.edu.br>
2021-10-14 16:20:10 -0300

    atualização durante a aula

--- sem10-11_Python/ex_pandas.py
+++ sem10-11_Python/ex_pandas.py
@@ -2,3 +2,7 @@
 # Semana 10, Curso PPGCS012
 # Importar e manipular os dados do dataset dos resumos da Anpuh com Pandas
 import pandas as pd
+
+df = pd.read_csv('CSVs/resumos_anpuh_2013-2021.csv', encoding='utf-8', index_col=0)
+
+print(df.head())
